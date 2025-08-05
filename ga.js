91eecb6cbd652b434552d63529a72664410e04cd
malu261290 <malu.261290@gmail.com>
2025-08-05 19:02:15 -0400

    Updates

---
+++ ga.js
@@ -0,0 +1,6 @@
+// Google Analytics 4 - Replace 'GA_MEASUREMENT_ID' with your actual measurement ID
+window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
+
+gtag('config', 'GA_MEASUREMENT_ID');
